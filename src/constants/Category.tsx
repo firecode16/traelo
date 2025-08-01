f96faa927040c9e7ed1cb1e35789adba9a62a7d8
FireCode <69737708+firecode16@users.noreply.github.com>
2025-08-01 00:57:32 -0600

    delete-trash
    delete all trash

--- src/constants/Category.tsx
+++
@@ -1,9 +0,0 @@
-export default {
-  cutlery: require('../assets/images/cutlery.png'),
-  fire: require('../assets/images/fire.png'),
-  star: require('../assets/images/star.png'),
-  cakes: require('../assets/images/categories/cakes.png'),
-  pizza: require('../assets/images/categories/pizza.png'),
-  chicken: require('../assets/images/categories/chicken.png'),
-  hamburger: require('../assets/images/categories/hamburger.png'),
-};
