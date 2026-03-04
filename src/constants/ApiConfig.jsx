a3db8e81ed921b779f9470672b52d2eeeec16e14
FireCode <69737708+firecode16@users.noreply.github.com>
2026-03-04 13:51:22 -0600

    Update BASE_URL for API configuration

--- src/constants/ApiConfig.jsx
+++ src/constants/ApiConfig.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://traelo-gateway-production.up.railway.app/api'; // 🔁 Change environment
+const BASE_URL = 'https://traelo-gateway-production.infra/api'; // 🔁 Change environment
 
 export { BASE_URL };
 
